c1e72e5843dc3e931c709f520abdfcf6edfe5e4c
Cosimo Kriegelstein <cosi@graswald-schaetzle.com>
2026-03-23 12:02:30 +0100

    Initial Commit

---
+++ cosmic-ios/cosmic_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  cosmic_iosApp.swift
+//  cosmic-ios
+//
+//  Created by Cosimo on 23.03.26.
+//
+
+import SwiftUI
+
+@main
+struct cosmic_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
